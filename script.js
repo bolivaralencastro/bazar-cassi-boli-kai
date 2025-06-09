785886ea41b169c195a2fcb979e34f3564cbce4f
Bolivar Alencastro <bolivar@alencastro.com.br>
2025-06-09 13:33:25 +0000

    chaves do supabase

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
     // --- CONFIGURAÇÕES GLOBAIS DA APLICAÇÃO ---
     const APP_CONFIG = {
         WHATSAPP_NUMBER: "5548984138601",
-        SUPABASE_URL: "__SUPABASE_URL__",
-        SUPABASE_ANON_KEY: "__SUPABASE_ANON_KEY__",
+        SUPABASE_URL: "https://jkxohciuzlxdccfxxizy.supabase.co", // <--- VALOR REAL AQUI
+        SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpreG9oY2l1emx4ZGNjZnh4aXp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMTUzOTksImV4cCI6MjA2NDg5MTM5OX0.gXGBTPT0dyxn6LH4JbTW8UUg6sb3dmomwHhZIfnr6zc", // <--- VALOR REAL AQUI
         LOCAL_STORAGE_LIKES_KEY: 'bazarV2_userLikedItems',
         PRODUCTS_JSON_PATH: 'data/products.json',
         DEFAULT_ERROR_MESSAGE: "<p class=\"error-message\">Desculpe, algo deu errado. Por favor, tente recarregar a página.</p>",
